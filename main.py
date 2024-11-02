623090be5c0245e59a88f94a0b81ce25694cc87d
bdelpozo <bdelpozo.del@gmail.com>
2024-11-02 12:17:40 +0100

    Revert "output message translated"
    This reverts commit bc2bfee99e46d84b98fa21a56ad6ab9244f70be9.

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ def roll(amount:int, sides:int):
     return dice.roll(f'{amount}d{sides}')
 
 for idx, result in enumerate(roll(5,6)):
-    print(f'The dice throwing number {idx+1} gets the number {result}')
+    print(f'Lanzamiento {idx+1} número obtenido {result}')
     sleep(5)
