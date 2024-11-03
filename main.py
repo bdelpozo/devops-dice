e6f0dd4a45a45a6ad02831ca2e82405dd5331319
bdelpozo <bdelpozo.del@gmail.com>
2024-11-03 12:34:41 +0100

    testing deploy failure message

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import sleep
 
 
 def roll(amount:int, sides:int):
-    return dice.roll(f'{amount}d{sides}')
+    return amount/0
 
 for idx, result in enumerate(roll(6,6)):
     print(f'Lanzamiento {idx+1} número obtenido {result}')
